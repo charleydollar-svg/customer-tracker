6bcc6dd949e71ef4d6e8584a508ea830a81a93cc
Charles <cholzschuher@usf.edu>
2026-02-05 18:45:41 -0500

    tweaked foreach list, updated charlie email

--- cc_3.js
+++ cc_3.js
@@ -1,15 +1,16 @@
 let customers = [
   {name: 'Alice', email: 'alice@example.com', purchases: 5 },
   {name: 'Bob', email: 'bob@example.com', purchases: 3 },
-  {name: 'Charlie', email: 'charlieburger@example.com', purchases: 8 },
+  {name: 'Charlie', email: 'charlie@example.com', purchases: 8 },
 ];
 
 customers.push({name: 'Diana', email: 'diana@example.com', purchases: 2}); //new customer
 customers.shift();
 
+customers[2].email = 'charlieburger@example.com';
 let newPurchase = 'purchases';
 customers[0][newPurchase] += 10; // Bob made a new purchase
 
-customers.forEach(myFunction);
-
-console.log()
+customers.forEach(customer => {
+  console.log(`${customer.name} (${customer.email}) has made ${customer.purchases} purchases.`);
+});
